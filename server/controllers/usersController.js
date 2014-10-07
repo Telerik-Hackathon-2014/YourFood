fd17fd275cf7d4fd961c825dd372027b775248f3
yasenm <ias_sv_bg@yahoo.co.uk>
2014-10-07 11:41:50 +0300

    Last fixes from yesterday

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,10 +4,9 @@ var User = require('mongoose').model('User'),
 module.exports = {
     createUser: function (req, res, next) {
         var newUserData = req.body;
-
         newUserData.salt = encryption.generateSalt();
-      //  console.log(newUserData.password)
         newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
+
         User.create(newUserData, function(err, user) {
             if (err) {
                 console.log('Failed to register new user: ' + err);
