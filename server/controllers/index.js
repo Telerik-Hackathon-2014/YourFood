7b63bc1bce1bd3bbc3d2a702bfee305a3b5ee3ce
yasenm <ias_sv_bg@yahoo.co.uk>
2014-10-07 12:12:37 +0300

    Recipes controller added and added reference to index.js for controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 var usersController = require('../controllers/usersController'),
     productsController = require('../controllers/productsController'),
-    catalogProductsController = require('../controllers/catalogProductsController');
+    catalogProductsController = require('../controllers/catalogProductsController'),
+    recipesController = require('../controllers/recipesController');
 
 module.exports = {
     users: usersController,
     products: productsController,
-    catalogProducts: catalogProductsController
+    catalogProducts: catalogProductsController,
+    recipes: recipesController
 };
---
+++ server/controllers/recipesController.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Recipe = require('mongoose').model('Recipe');
+
+module.exports = {
+    createRecipe: function (req, res, next) {
+        console.log(req);
+        var newRecipeData = req.body;
+
+        Recipe.create(newRecipeData, function (err, product) {
+            if (err) {
+                console.log('Failed to create recipe: ' + err);
+                return;
+            }
+
+            res.send(product);
+            res.end();
+        });
+    },
+    updateRecipe: function (req, res) {
+        var newRecipeData = req.body;
+
+        if (req.body._id && Product.findOne({_id: req.body._id}) ) {
+            Recipe.update({_id: req.body._id}, newRecipeData, function () {
+                res.send(newRecipeData);
+                res.end();
+            });
+        }
+    },
+    getAllRecipes: function (req, res) {
+        Recipe.find({}).exec(function (err, collection) {
+            if (err) {
+                console.log('Trying to get all recipes did not work out: ' + err);
+                return;
+            }
+
+            res.send(collection);
+        });
+    },
+    // TODO: Must add to Recipe model person that creates the recipe so we can filter them
+    getRecipesByUse: function (user) {
+
+    }
+};
