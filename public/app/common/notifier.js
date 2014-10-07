712319bf4a79764978b67598e0d543a46e50a2e3
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-10-07 19:17:00 +0300

    Comment toastr

--- public/app/common/notifier.js
+++ public/app/common/notifier.js
@@ -1,10 +1,10 @@
 app.factory('notifier', function(toastr){
     return {
         success: function(msg){
-            toastr.success(msg);
+          //  toastr.success(msg);
         },
         error: function(msg){
-            toastr.error(msg);
+           // toastr.error(msg);
         }
     }
 });
