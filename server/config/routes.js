70a7a37d8ed3f3a9ad46e7c614d0dc0a88813621
yasenm <ias_sv_bg@yahoo.co.uk>
2014-10-07 11:57:54 +0300

    Routing for products and catalog products done with authentication needed

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,20 +1,21 @@
 var auth = require('./auth'),
     controllers = require('../controllers');
 
-module.exports = function(app){
+module.exports = function (app) {
 
     app.get('/api/users', auth.isInRole('admin'), controllers.users.getAllUsers);
-
     app.post('/api/users', controllers.users.createUser);
-
     app.put('/api/users', auth.isAuthenticated, controllers.users.updateUser);
 
-    app.get('/api/products', controllers.products.getAllProducts);
-    app.post('/api/products', controllers.products.createProduct);
+    app.get('/api/products', auth.isAuthenticated, controllers.products.getAllProducts);
+    app.post('/api/products', auth.isAuthenticated, controllers.products.createProduct);
+    app.put('/api/products', auth.isAuthenticated, controllers.products.updateProduct);
 
-    app.get('/api/catalog-products', controllers.catalogProducts.getAllCatalogProducts);
+    app.get('/api/catalog-products', auth.isAuthenticated, controllers.catalogProducts.getAllCatalogProducts);
+    app.post('/api/catalog-products', auth.isAuthenticated, controllers.catalogProducts.createCatalogProduct);
+    app.put('/api/catalog-products', auth.isAuthenticated, controllers.catalogProducts.updateCatalogProduct);
 
-    app.get('/partials/:partialDir/:partialName', function(req, res){
+    app.get('/partials/:partialDir/:partialName', function (req, res) {
         res.render('../../public/app/' + req.params.partialDir + '/' + req.params.partialName);
     });
 
@@ -22,13 +23,13 @@ module.exports = function(app){
 
     app.post('/logout', auth.logout);
 
-    
+
     app.get('/api/*', function (req, res) {
         res.status(404);
         res.end();
     });
 
-    app.get('*', function(req, res){
+    app.get('*', function (req, res) {
         res.render('index', {currentUser: req.user});
     });
 };
