bb792dc2fbffcbb9fe494634d9786fc206e73c9a
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-10-08 14:23:24 +0300

    Add sample recipes filter

--- public/app/recipes/recipesCtrl.js
+++ public/app/recipes/recipesCtrl.js
@@ -5,10 +5,31 @@ app.controller('RecipesCtrl',
 
         $scope.isLogged = identity.isAuthenticated();
 
-        $scope.test = 'HERE!';
-
         recipesData.getAllRecipes(
             function (data) {
                 $scope.recipes = data;
             });
+
+        $scope.page = $scope.page || 1;
+
+        $scope.nextPage = function () {
+            $scope.page++;
+        };
+
+        $scope.prevPage = function () {
+            if ($scope.page > 1) {
+                $scope.page--;
+            }
+        };
+
+        $scope.oneAtATime = true;
+
+        $scope.status = {
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
+
+        $scope.sort = function () {
+            // TODO ADD FILTERS
+        };
     });
